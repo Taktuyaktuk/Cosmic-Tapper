9ce48446a717b24eca196fc187e9dec45874488a
Jaroslaw Gawel <audikrk@interia.pl>
2022-05-05 19:12:52 +0200

    Final Version

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameOverConfirmed;
+
+    public static GameManager Instance;
+
+    public GameObject StartPage;
+    public GameObject GameOverPage;
+    public GameObject CountDownPage;
+    public Text scoreText;
+    
+
+    enum PageState
+    {
+        None,
+        Start,
+        GameOver,
+        Countdown
+    }
+
+    int score = 0;
+    bool gameOver = true;
+
+    public bool GameOver { get { return gameOver; } }
+    public int Score { get { return score; } }
+
+    void Awake()
+    {
+       Instance = this;
+    }
+
+    void OnEnable()
+    {
+        CountdownText.OnCountdownFinished += OnCountdownFinished;
+        PlayerControler.OnPlayerDied += OnPlayerDied;
+        PlayerControler.OnPlayerScored += OnPlayerScored;
+    }
+
+    void OnDisable()
+    {
+        CountdownText.OnCountdownFinished -= OnCountdownFinished;
+        PlayerControler.OnPlayerDied -= OnPlayerDied;
+        PlayerControler.OnPlayerScored -= OnPlayerScored;
+    }
+
+    void OnCountdownFinished()
+    {
+        SetPageState(PageState.None);
+        OnGameStarted();
+        score = 0;
+        gameOver = false;
+    }
+
+    void OnPlayerDied()
+    {
+        gameOver = true;
+        int savedScore = PlayerPrefs.GetInt("HighScore");
+        if (score > savedScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+
+        SetPageState(PageState.GameOver);
+    }
+
+        void OnPlayerScored()
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
+    
+
+   
+
+    void SetPageState(PageState state)
+    {
+        switch (state)
+        {
+            case PageState.None:
+                StartPage.SetActive(false);
+                GameOverPage.SetActive(false);
+                CountDownPage.SetActive(false);
+
+                break;
+            case PageState.Start:
+                StartPage.SetActive(true);
+                GameOverPage.SetActive(false);
+                CountDownPage.SetActive(false);
+
+                break;
+            case PageState.GameOver:
+                StartPage.SetActive(false);
+                GameOverPage.SetActive(true);
+                CountDownPage.SetActive(false);
+
+                break;
+            case PageState.Countdown:
+                StartPage.SetActive(false);
+                GameOverPage.SetActive(false);
+                CountDownPage.SetActive(true);
+
+                break;
+        }
+    }
+
+    public void ConfirmGameOver()
+    {
+        OnGameOverConfirmed();
+        scoreText.text = "0";
+        SetPageState(PageState.Start);
+    }
+
+    public void StartGame()
+    {
+        SetPageState(PageState.Countdown);
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerControler : MonoBehaviour
+{
+    public delegate void PlayerDelegate();
+    public static event PlayerDelegate OnPlayerDied;
+    public static event PlayerDelegate OnPlayerScored;
+
+    public Vector3 startPos;
+    public float tSmooth = 5;
+    public float tapPower = 10;
+    private Animator anim;
+
+    public AudioSource tapAudio;
+    public AudioSource scoreAudio;
+    public AudioSource dieAudio;
+   
+
+    Rigidbody2D rigidbody;
+    Quaternion forwardRotation;
+    Quaternion downRotation;
+    GameManager game;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        forwardRotation = Quaternion.Euler(0, 0, 25);
+        downRotation = Quaternion.Euler(0, 0, -70);
+        game = GameManager.Instance;
+        rigidbody.simulated = false;
+
+    }
+
+    void OnEnable()
+    {
+        GameManager.OnGameStarted += OnGameStarted;
+        GameManager.OnGameOverConfirmed += OnGameOverConfirmed;
+    }
+
+    void OnDisable()
+    {
+        GameManager.OnGameStarted -= OnGameStarted;
+        GameManager.OnGameOverConfirmed -= OnGameOverConfirmed;
+
+    }
+
+    void OnGameStarted()
+    {
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.simulated = true;
+    }
+
+    void OnGameOverConfirmed()
+    {
+        transform.localPosition = startPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+    void Update()
+    {
+        if (game.GameOver) return;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            tapAudio.Play();
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector2.up * tapPower, ForceMode2D.Force);
+        }
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tSmooth * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "ScoreArea")
+        {
+            OnPlayerScored();
+            scoreAudio.Play();
+        }
+
+        if(col.gameObject.tag == "DeadZone")
+        {
+            rigidbody.simulated = false;
+
+            OnPlayerDied();
+            dieAudio.Play();
+        }
+    }
+
+    
+}
